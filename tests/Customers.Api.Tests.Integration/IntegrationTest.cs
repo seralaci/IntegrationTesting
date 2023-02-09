4598a05f52ecef7263ab375c0b1ba3905eb6a777
László Séra <laszlo.sera@gmail.com>
2023-02-09 17:18:53 +0100

    First integration test

---
+++ tests/Customers.Api.Tests.Integration/IntegrationTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Net;
+using Xunit;
+
+namespace Customers.Api.Tests.Integration;
+
+public class IntegrationTest
+{
+    [Fact]
+    public async Task Test()
+    {
+        var httpClient = new HttpClient
+        {
+            BaseAddress = new Uri("https://localhost:5001")
+        };
+
+        var response = await httpClient.GetAsync($"customers/{Guid.NewGuid()}");
+
+        Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+    }
+}
