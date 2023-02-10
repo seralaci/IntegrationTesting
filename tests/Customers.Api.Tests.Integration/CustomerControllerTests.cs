aaea89bc6fc3fee94f6f7ff9c951fb2d6f28e47f
László Séra <laszlo.sera@gmail.com>
2023-02-10 14:22:40 +0100

    MemberData-based parametrization

--- tests/Customers.Api.Tests.Integration/CustomerControllerTests.cs
+++ tests/Customers.Api.Tests.Integration/CustomerControllerTests.cs
@@ -18,7 +18,19 @@ public class CustomerControllerTests : IAsyncLifetime, IDisposable
     [Theory]
     [InlineData("2EFD541F-3A80-414E-8FE5-524C93D58379")]
     [InlineData("19C1DBD6-7B12-4A4A-8D56-51736E7670D2")]
-    public async Task Get_ReturnsNotFound_WhenCustomerDoesNotExists(string guidAsText)
+    public async Task Get_ReturnsNotFound_WhenCustomerDoesNotExists_InlineDataParameters(string guidAsText)
+    {
+        // Arrange
+        // Act
+        var response = await _httpClient.GetAsync($"customers/{Guid.Parse(guidAsText)}");
+
+        // Assert
+        Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+    }
+    
+    [Theory]
+    [MemberData(nameof(Data))]
+    public async Task Get_ReturnsNotFound_WhenCustomerDoesNotExists_MemberDataParameters(string guidAsText)
     {
         // Arrange
         // Act
@@ -45,4 +57,10 @@ public class CustomerControllerTests : IAsyncLifetime, IDisposable
         // Sync cleanup
         _httpClient.Dispose();
     }
+
+    public static IEnumerable<object[]> Data { get; } = new[]
+    {
+        new[] {"2EFD541F-3A80-414E-8FE5-524C93D58379"},
+        new[] {"19C1DBD6-7B12-4A4A-8D56-51736E7670D2"}
+    };
 }
