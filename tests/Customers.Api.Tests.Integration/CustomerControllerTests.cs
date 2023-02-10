a36d99f6d0ef7bfff12831948e5e8b6ea938bebd
László Séra <laszlo.sera@gmail.com>
2023-02-10 09:45:26 +0100

    Arrange - Act - Assert

--- tests/Customers.Api.Tests.Integration/CustomerControllerTests.cs
+++ tests/Customers.Api.Tests.Integration/CustomerControllerTests.cs
@@ -8,13 +8,16 @@ public class CustomerControllerTests
     [Fact]
     public async Task Get_ReturnsNotFound_WhenCustomerDoesNotExists()
     {
+        // Arrange
         var httpClient = new HttpClient
         {
             BaseAddress = new Uri("https://localhost:5001")
         };
 
+        // Act
         var response = await httpClient.GetAsync($"customers/{Guid.NewGuid()}");
 
+        // Assert
         Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
     }
 }
