e925dc7c8510980d1d784659315c38c7d00df8ae
László Séra <laszlo.sera@gmail.com>
2023-02-10 14:19:40 +0100

    Parametrized test

--- tests/Customers.Api.Tests.Integration/CustomerControllerTests.cs
+++ tests/Customers.Api.Tests.Integration/CustomerControllerTests.cs
@@ -15,12 +15,14 @@ public class CustomerControllerTests : IAsyncLifetime, IDisposable
         //  Sync Setup
     }
 
-    [Fact]
-    public async Task Get_ReturnsNotFound_WhenCustomerDoesNotExists()
+    [Theory]
+    [InlineData("2EFD541F-3A80-414E-8FE5-524C93D58379")]
+    [InlineData("19C1DBD6-7B12-4A4A-8D56-51736E7670D2")]
+    public async Task Get_ReturnsNotFound_WhenCustomerDoesNotExists(string guidAsText)
     {
         // Arrange
         // Act
-        var response = await _httpClient.GetAsync($"customers/{Guid.NewGuid()}");
+        var response = await _httpClient.GetAsync($"customers/{Guid.Parse(guidAsText)}");
 
         // Assert
         Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
