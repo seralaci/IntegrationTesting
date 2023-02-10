51371edaf4a81ff79206377aee56dff0701c44c5
László Séra <laszlo.sera@gmail.com>
2023-02-10 09:55:35 +0100

    setup and cleanup

--- tests/Customers.Api.Tests.Integration/CustomerControllerTests.cs
+++ tests/Customers.Api.Tests.Integration/CustomerControllerTests.cs
@@ -3,21 +3,44 @@ using Xunit;
 
 namespace Customers.Api.Tests.Integration;
 
-public class CustomerControllerTests
+public class CustomerControllerTests : IAsyncLifetime, IDisposable
 {
+    private readonly HttpClient _httpClient = new()
+    {
+        BaseAddress = new Uri("https://localhost:5001")
+    };
+
+    public CustomerControllerTests()
+    {
+        //  Sync Setup
+    }
+
     [Fact]
     public async Task Get_ReturnsNotFound_WhenCustomerDoesNotExists()
     {
         // Arrange
-        var httpClient = new HttpClient
-        {
-            BaseAddress = new Uri("https://localhost:5001")
-        };
-
         // Act
-        var response = await httpClient.GetAsync($"customers/{Guid.NewGuid()}");
+        var response = await _httpClient.GetAsync($"customers/{Guid.NewGuid()}");
 
         // Assert
         Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+    }
+
+    public Task InitializeAsync()
+    {
+        // Async Setup
+        return Task.CompletedTask;
+    }
+
+    public Task DisposeAsync()
+    {
+        // Async cleanup
+        return Task.CompletedTask;
+    }
+
+    public void Dispose()
+    {
+        // Sync cleanup
+        _httpClient.Dispose();
     }
 }
