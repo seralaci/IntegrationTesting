b42cee667b73e678640710f031ac8e9833adf280
László Séra <laszlo.sera@gmail.com>
2023-02-15 16:38:41 +0100

    Introducing WireMock

--- tests/Customers.Api.Tests.Integration/CustomerApiFactory.cs
+++ tests/Customers.Api.Tests.Integration/CustomerApiFactory.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Logging;
+using Microsoft.Net.Http.Headers;
 using Xunit;
 
 namespace Customers.Api.Tests.Integration;
@@ -31,6 +32,9 @@ public class CustomerApiFactory : WebApplicationFactory<IApiMarker>, IAsyncLifet
             Password = "changeme",
         })
         .Build();
+
+    private readonly GitHubApiServer _gitHubApiServer = new();
+    
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
         builder.ConfigureLogging(logging =>
@@ -42,16 +46,29 @@ public class CustomerApiFactory : WebApplicationFactory<IApiMarker>, IAsyncLifet
         {
             services.RemoveAll(typeof(IDbConnectionFactory));
             services.AddSingleton<IDbConnectionFactory>(_ => new NpgsqlConnectionFactory(_dbContainer.ConnectionString));
+            
+            
+            services.AddHttpClient("GitHub", httpClient =>
+            {
+                httpClient.BaseAddress = new Uri(_gitHubApiServer.Url);
+                httpClient.DefaultRequestHeaders.Add(HeaderNames.Accept, "application/vnd.github.v3+json");
+                httpClient.DefaultRequestHeaders.Add(HeaderNames.UserAgent, $"Course-{Environment.MachineName}");
+            });
         });
     }
 
     public async Task InitializeAsync()
     {
+        _gitHubApiServer.Start();
+        _gitHubApiServer.SetupUser("johndoe");
+        
         await _dbContainer.StartAsync();
     }
 
     public new async Task DisposeAsync()
     {
         await _dbContainer.DisposeAsync();
+        
+        _gitHubApiServer.Dispose();
     }
 }
