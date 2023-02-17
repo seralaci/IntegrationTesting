c42f837a0087786bc1625828a15bde0f293d9850
László Séra <laszlo.sera@gmail.com>
2023-02-17 18:55:33 +0100

    Testing: Delete customer

---
+++ tests/Customers.Api.Tests.Integration/CustomerController/DeleteCustomerControllerTests.cs
@@ -0,0 +1,52 @@
+﻿using System.Net;
+using System.Net.Http.Json;
+using Bogus;
+using Customers.Api.Contracts.Requests;
+using Customers.Api.Contracts.Responses;
+using FluentAssertions;
+using Xunit;
+
+namespace Customers.Api.Tests.Integration.CustomerController;
+
+public class DeleteCustomerControllerTests: IClassFixture<CustomerApiFactory>
+{
+    private readonly HttpClient _httpClient;
+
+    private readonly Faker<CustomerRequest> _customerGenerator = new Faker<CustomerRequest>()
+        .RuleFor(x => x.FullName, faker => faker.Person.FullName)
+        .RuleFor(x => x.Email, faker => faker.Person.Email)
+        .RuleFor(x => x.GitHubUsername, CustomerApiFactory.ValidGitHubUser)
+        .RuleFor(x => x.DateOfBirth, faker => faker.Person.DateOfBirth.Date);
+
+    public DeleteCustomerControllerTests(CustomerApiFactory apiFactory)
+    {
+        //  Sync Setup
+        _httpClient = apiFactory.CreateClient();
+    }
+
+    [Fact]
+    public async Task Delete_ReturnsOk_WhenCustomerExists()
+    {
+        // Arrange
+        var customer = _customerGenerator.Generate();
+        var createdResponse = await _httpClient.PostAsJsonAsync("customers", customer);
+        var createdCustomer = await createdResponse.Content.ReadFromJsonAsync<CustomerResponse>();
+
+        // Act
+        var response = await _httpClient.DeleteAsync($"customers/{createdCustomer!.Id}");
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+    }
+
+    [Fact]
+    public async Task Delete_ReturnsNotFound_WhenCustomerDoesNotExist()
+    {
+        // Arrange
+        // Act
+        var response = await _httpClient.DeleteAsync($"customers/{Guid.NewGuid()}");
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+    }
+}
