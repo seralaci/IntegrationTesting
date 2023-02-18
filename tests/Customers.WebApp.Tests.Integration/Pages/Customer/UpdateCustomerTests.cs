1dc3cc28549deb4132323751b08229659bec3301
László Séra <laszlo.sera@gmail.com>
2023-02-18 13:45:07 +0100

    UI test: Update a customer

--- tests/Customers.WebApp.Tests.Integration/Pages/Customer/UpdateCustomerTests.cs
+++ tests/Customers.WebApp.Tests.Integration/Pages/Customer/UpdateCustomerTests.cs
@@ -7,7 +7,7 @@ using Xunit;
 namespace Customers.WebApp.Tests.Integration.Pages.Customer;
 
 [Collection("Test collection")]
-public class AddCustomerTests
+public class UpdateCustomerTests
 {
     private readonly SharedTestContext _testContext;
     
@@ -17,28 +17,25 @@ public class AddCustomerTests
         .RuleFor(x => x.GitHubUsername, SharedTestContext.ValidGitHubUser)
         .RuleFor(x => x.DateOfBirth, faker => DateOnly.FromDateTime(faker.Person.DateOfBirth.Date));
 
-    public AddCustomerTests(SharedTestContext testContext)
+    public UpdateCustomerTests(SharedTestContext testContext)
     {
         _testContext = testContext;
     }
 
     [Fact]
-    public async Task Create_CreateCustomer_WhenDataIsValid()
+    public async Task Update_UpdateCustomer_WhenDataIsValid()
     {
         // Arrange
         var page = await _testContext.Browser.NewPageAsync(new BrowserNewPageOptions
         {
             BaseURL = SharedTestContext.AppUrl
         });
-        await page.GotoAsync("add-customer");
-        var customer = _customerGenerator.Generate();
+        var customer = await CreateCustomer(page);
+        await page.GotoAsync($"/update-customer/{customer.Id}");
+        customer.FullName = "Another Name";
 
         // Act
         await page.FillAsync("input[id=fullname]", customer.FullName);
-        await page.FillAsync("input[id=email]", customer.Email);
-        await page.FillAsync("input[id=github-username]", customer.GitHubUsername);
-        await page.FillAsync("input[id=dob]", customer.DateOfBirth.ToString("yyyy-MM-dd"));
-
         await page.ClickAsync("button[type=submit]");
 
         // Assert
@@ -53,29 +50,48 @@ public class AddCustomerTests
 
         await page.CloseAsync();
     }
-
+    
     [Fact]
-    public async Task Create_ShowsError_WhenEmailIsInvalid()
+    public async Task Update_ShowsError_WhenEmailIsInvalid()
     {
         // Arrange
         var page = await _testContext.Browser.NewPageAsync(new BrowserNewPageOptions
         {
             BaseURL = SharedTestContext.AppUrl
         });
-        await page.GotoAsync("add-customer");
-        var customer = _customerGenerator.Generate();
-        
+        var customer = await CreateCustomer(page);
+        await page.GotoAsync($"/update-customer/{customer.Id}");
+        customer.Email = "invalidEmail";
+
         // Act
-        await page.FillAsync("input[id=fullname]", customer.FullName);
-        await page.FillAsync("input[id=email]", "invalidEmail");
-        await page.FillAsync("input[id=github-username]", customer.GitHubUsername);
-        await page.FillAsync("input[id=dob]", customer.DateOfBirth.ToString("yyyy-MM-dd"));
+        await page.FillAsync("input[id=email]", customer.Email);
+        await page.FocusAsync("input[id=fullname]");
         
         // Assert
         var element = page.Locator("li.validation-message").First;
         var text = await element.InnerTextAsync();
         text.Should().Be("Invalid email format");
-        
+
         await page.CloseAsync();
+    }
+    
+    private async Task<Models.Customer> CreateCustomer(IPage page)
+    {
+        await page.GotoAsync("add-customer");
+        var customer = _customerGenerator.Generate();
+
+        await page.FillAsync("input[id=fullname]", customer.FullName);
+        await page.FillAsync("input[id=email]", customer.Email);
+        await page.FillAsync("input[id=github-username]", customer.GitHubUsername);
+        await page.FillAsync("input[id=dob]", customer.DateOfBirth.ToString("yyyy-MM-dd"));
+
+        await page.ClickAsync("button[type=submit]");
+        
+        var linkElement = page.Locator("article>p>a").First;
+        var link = await linkElement.GetAttributeAsync("href");
+        var idInText = link!.Split('/').Last();
+        customer.Id = Guid.Parse(idInText);
+
+        return customer;
     }
 }
